6c160d1dc92f885b1552ac5bba961f4034b88d5f
Nasit Sarwar Sony <nasitsony96@gmail.com>
2021-10-01 08:06:18 -0700

    Update canSum.cpp

--- canSum.cpp
+++ canSum.cpp
@@ -9,3 +9,21 @@ bool canSum(int target, vector<int> &numbers){
       
       return false;
 }
+
+
+ bool canSumMemo(int target, vector<int> &numbers, unordered_map<int, bool> &memo){
+    
+    if(memo.find(target) != memo.end()) return memo[target];
+   
+    if(target == 0) return true;
+    if(target < 0) return false;
+    
+    
+    for(int x:numbers)
+      if(canSumMemo(target-x, numbers, memo)){ 
+        memo[target] = true;  
+        return true;
+      }
+       memo[target] = false;  
+      return false;
+}
